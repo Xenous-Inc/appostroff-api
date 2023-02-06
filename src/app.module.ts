48eedd9a9bd4f3fed08ed1ff1dcdf43bbe9e0b03
slipneff <88764276+slipneff@users.noreply.github.com>
2023-02-06 17:21:41 +0300

    fix-861manmma/environments-support add support for different environments

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,8 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import { configuration } from '../config/configuration';
-import { validationSchema } from '../config/validation';
+import { configuration } from './config/configuration';
+import { validationSchema } from './config/validation';
 @Module({
     imports: [
         ConfigModule.forRoot({
