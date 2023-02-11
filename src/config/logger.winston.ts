63fe3279518b61c20ae46f504a1b8154d35d1fcb
slipneff <88764276+slipneff@users.noreply.github.com>
2023-02-11 17:52:49 +0300

    fix-861mb9984/logger add pretty print and id

--- src/config/logger.winston.ts
+++ src/config/logger.winston.ts
@@ -1,16 +1,22 @@
 import { createLogger, format, transports } from 'winston';
-const { combine, splat, timestamp, printf } = format;
+import * as colors from 'colors';
 
 // eslint-disable-next-line @typescript-eslint/no-shadow
-const myFormat = printf(({ level, message, timestamp, ...metadata }) => {
-    let msg = `${timestamp} [${level}] : ${message} `;
-    if (metadata) {
-        msg += JSON.stringify(metadata);
-    }
-    return msg;
+const logger_format = format.printf(data => {
+    const timestamp = colors.magenta(`${data.timestamp}`);
+    return `[${timestamp}]  ${colors.bold(data.level)}: ${colors.cyan(data.message)}`;
 });
-
+const { combine, splat, timestamp } = format;
 export const logger = createLogger({
-    format: combine(format.colorize(), splat(), timestamp(), myFormat),
+    format: combine(
+        format(info => {
+            info.level = info.level.toUpperCase();
+            return info;
+        })(),
+        format.colorize(),
+        splat(),
+        timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+        logger_format,
+    ),
     transports: [new transports.Console({ level: 'info' })],
 });
--- src/middleware/logger.middleware.ts
+++ src/middleware/logger.middleware.ts
@@ -1,13 +1,15 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
 import { logger } from '../config/logger.winston';
+import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
     use(req: Request, res: Response, next: NextFunction) {
         try {
-            const { ip, baseUrl } = req;
-            logger.info(`IP: ${ip} HTTP Route:${baseUrl}`);
+            const { ip, baseUrl, headers } = req;
+            const id = headers['x-request-id'] || uuidv4();
+            logger.info(`IP: "${ip}", Route: "${baseUrl}", ID: "${id}"`);
         } catch (e) {
             logger.error(JSON.stringify(e));
         }
